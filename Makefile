d941c92f35be5ca764cf27770655c71180de29f8
Takahito Iwano <iwataka369167.kuma@gmail.com>
2025-12-18 22:07:03 +0900

    clean up

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: all help init
+
+all: help
+
+help:
+	@echo "make init          #=> Run init scripts"
+
+init:
+	./init.sh
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+set SCRIPT_PATH (status current-filename)
+set SCRIPT_DIR (cd (dirname $SCRIPT_PATH); and pwd)
+
+set DOTFILES_DIR (cd "$SCRIPT_DIR"; and pwd)
+
+echo $DOTFILES_DIR
+
+mkdir -p ~/.config
+ln -nsf "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
