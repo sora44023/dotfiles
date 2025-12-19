172d2023b9a5c2e146e07a7fad5b51f7eece5fcd
Takahito Iwano <iwataka369167.kuma@gmail.com>
2025-12-19 17:43:57 +0900

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 local keymap = vim.keymap.set
 
+keymap('n', '<C-a>', '<Home>')
+keymap('n', '<C-e>', '<End>')
+keymap('n', '<C-b>', '<Left>')
+keymap('n', '<C-f>', '<Right>')
+keymap('n', '<C-d>', '<Del>')
+
 keymap("i", "<S-Tab>", "<C-d>", { noremap = true, silent = true })
+keymap('i', '<C-a>', '<Home>')
+keymap('i', '<C-e>', '<End>')
+keymap('i', '<C-b>', '<Left>')
+keymap('i', '<C-f>', '<Right>')
+keymap('i', '<C-d>', '<Del>')
